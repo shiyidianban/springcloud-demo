2e830c01dc8db67df41147ae51b46bcf2c302e8c
武继飞 <wujifei@thundersdata.com>
2020-05-06 10:16:15 +0800

    修改bug

--- springcloud-moudle/springcloud-client/src/main/java/com/springcloud/demo/client/entity/Client.java
+++ springcloud-moudle/springcloud-client/src/main/java/com/springcloud/demo/client/entity/Client.java
@@ -4,9 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -19,9 +16,6 @@ import java.time.LocalDateTime;
  * @author FLY
  * @since 2020-04-30
  */
-@Data
-@EqualsAndHashCode(callSuper = false)
-@Accessors(chain = true)
 @TableName("client")
 public class Client implements Serializable {
 
@@ -64,4 +58,55 @@ public class Client implements Serializable {
     private LocalDateTime updateTime;
 
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientPath() {
+        return clientPath;
+    }
+
+    public void setClientPath(String clientPath) {
+        this.clientPath = clientPath;
+    }
+
+    public Integer getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
 }
