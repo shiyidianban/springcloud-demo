133ce66ea1a0c1967646df9fa1dc8f215267efa1
武继飞 <wujifei@thundersdata.com>
2020-05-22 10:27:35 +0800

    添加八门

--- springcloud-moudle/springcloud-metaphysics/src/main/java/com/springcloud/demo/metaphysics/enums/BA_GUA.java
+++ springcloud-moudle/springcloud-metaphysics/src/main/java/com/springcloud/demo/metaphysics/enums/BA_GUA.java
@@ -12,15 +12,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum BA_GUA {
-
-    QIAN("乾", "天行健，君子以自强不息", "乾三连", WU_XING.XI_BEI),
-    KUN("坤", "地势坤，君子以厚德载物", "坤六断", WU_XING.XI_NAN),
-    XUN("巽", "随风巽，君子以申命行事", "巽下断", WU_XING.DONG_NAN),
-    ZHEN("震", "渐雷震，君子以恐惧修省", "震仰盂", WU_XING.MU),
-    KAN("坎", "善如水，君子以作事谋始", "坎中满", WU_XING.SHUI),
-    LI("离", "火同人，君子以类族辨物", "离中虚", WU_XING.HUO),
-    DUI("兑", "步泽履，君子以辨民安志", "兑上缺", WU_XING.JIN),
-    GEN("艮", "艮山谦，君子以裒多益寡", "艮覆碗", WU_XING.DONG_BEI);
+    KAN("坎", "善如水，君子以作事谋始", "坎中满", "休", WU_XING.SHUI),
+    GEN("艮", "艮山谦，君子以裒多益寡", "艮覆碗", "生", WU_XING.DONG_BEI),
+    ZHEN("震", "渐雷震，君子以恐惧修省", "震仰盂", "伤", WU_XING.MU),
+    XUN("巽", "随风巽，君子以申命行事", "巽下断", "杜", WU_XING.DONG_NAN),
+    LI("离", "火同人，君子以类族辨物", "离中虚", "景", WU_XING.HUO),
+    KUN("坤", "地势坤，君子以厚德载物", "坤六断", "死", WU_XING.XI_NAN),
+    DUI("兑", "步泽履，君子以辨民安志", "兑上缺", "惊", WU_XING.JIN),
+    QIAN("乾", "天行健，君子以自强不息", "乾三连", "开", WU_XING.XI_BEI),
+    ;
 
     /**
      * 值
@@ -30,6 +30,8 @@ public enum BA_GUA {
     private String desc;
 
     private String kouJue;
+
+    private String baMen;
 
     private WU_XING wuXing;
 
